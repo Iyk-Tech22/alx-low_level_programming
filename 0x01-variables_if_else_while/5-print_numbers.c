8c980e092ed1261c0e7b4f4bbb1eb584e18c6d54
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-01 16:21:53 -0700

    added c file for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point of the program
+ *
+ * Return: 0 if there were no errors
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
