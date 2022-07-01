4d5549c152b41bbc43ec89b4a1b6ebf4c853a3b6
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-01 14:53:18 -0700

    added c file for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point to the program
+ *
+ * Return: 0 when there are no error
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
