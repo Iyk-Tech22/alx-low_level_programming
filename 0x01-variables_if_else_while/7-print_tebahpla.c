79004a3313feb9235f8b09df249eb3f982c339d5
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-02 07:49:45 -0700

    added c file task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 is if there were no error
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
