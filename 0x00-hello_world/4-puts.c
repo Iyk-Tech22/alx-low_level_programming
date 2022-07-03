2515b9be3c3e6c97ec16e03e362ff221fa5c5775
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-03 14:57:34 -0700

    added script file for task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if there were no error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
