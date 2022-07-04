564d1fba922fd929b767deed021869190b6cf587
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-04 04:04:17 -0700

    updated c file task 101 qoute advc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,9 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fwrite(str, 1, sizeof(str), stderr);
+	fwrite("\n", 1, 1, stderr);
 	return (1);
 }
