ce0c55022d66f10914dcc42ec49bbd7c7a14f786
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-04 05:21:55 -0700

    updated c file for task 101 qoute adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of the program
@@ -7,9 +8,8 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	fwrite(str, 1, sizeof(str), stderr);
-	fwrite("\n", 1, 1, stderr);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
