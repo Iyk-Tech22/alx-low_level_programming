86a979f5204a71939b86efc199a8dd56c4b0a58b
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-05 21:12:43 -0700

    added c files for task  5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#incude "main.h"
+
+/**
+ * print_sign - It Print sign of a number
+ * @n: input to get sign from
+ *
+ * Return: 1 if pos or 0 if zero or -1 if nev
+ */
+int print_sign(int n)
+{
+	int num;
+
+	num = 0;
+	if (n  > num)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == num)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}	
