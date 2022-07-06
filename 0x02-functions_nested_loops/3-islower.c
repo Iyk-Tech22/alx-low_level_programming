666da10b020786c5b3ef4e453104f7728ff70ebd
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-05 18:25:43 -0700

    added c file task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checks data if it lowercase
+ * @c: character to be check
+ *
+ * Return: 1 for true or 0 for false
+ */
+int _islower(char c)
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph == c)
+			return (1);
+		else
+			return (0);
+	}
+}
