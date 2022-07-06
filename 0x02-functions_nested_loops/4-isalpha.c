7bc0809f3a7f968fd14454d8a7fa45d140333586
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-05 19:52:26 -0700

    added c file task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks input for all alph case
+ * @c: input data to be check
+ *
+ * Return: 1 if true or 0 in it not a alph
+ */
+int _isalpha(char c)
+{
+	char a, A;
+	int found;
+
+	found = 0;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == c)
+		{
+			found = 1;
+			break;
+		}
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		if (a != c && A == c)
+		{
+			found = 1;
+			break;
+		}
+		else
+			break;
+	}
+	if (found)
+		return (1);
+	return (0);
+}
+
