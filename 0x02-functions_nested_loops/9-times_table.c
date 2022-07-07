fadc312a5c1abe98cb4719d714c64d0829976443
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-06 17:49:01 -0700

    added c files for task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ " times_table - print a times table of 0 - 9
+ */
+void times_table(void)
+{
+	int n, b;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			_putchar((n*b) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
