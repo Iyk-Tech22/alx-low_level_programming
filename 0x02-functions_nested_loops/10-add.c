b3a3b903006016f29d27d9d0e854920be8b55a12
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-06 19:07:28 -0700

    added c files for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @num1: input data to add
+ * @num2: input 2 data to add
+ *
+ * Return: sum of the two inputs
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return(sum);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if there were
+ */
+int main(void)
+{
+	int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
