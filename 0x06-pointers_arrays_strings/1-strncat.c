50e1c31c1312405838229a3f2b57c143af53792e
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-15 17:41:27 -0700

    update 1-strncat.c file for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int destlen;
 	int i;
 	char *destp;
 
-	destlen = 0;
 	destp = dest;
 	while (*dest != '\0')
 	{
-		destlen++;
 		dest++;
 	}
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[destlen + i] = src[i];
-	dest[destlen + 1] = '\0';
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 	return (destp);
 }
