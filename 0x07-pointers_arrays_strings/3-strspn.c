c5891eb28b9ae1550afb59cc9759bb2ad0c08ab8
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-19 16:43:00 -0700

    Create 3-strspn.c file task 3 for 0x07 project

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - Get the length of a substring in a string
+ * @s: Input string to check for march
+ * @accept: Substring to check for march
+ *
+ * Return: Length of substring in the string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	char *strp;
+	unsigned int n;
+
+	n = 0;
+	while (*accept != '\0')
+	{
+		strp = s;
+		while (*strp != '\0')
+		{
+			if (*strp == *accept)
+			{
+				n++;
+				break;
+			}
+			strp++;
+		}
+		accept++;
+	}
+	return (n);
+}
