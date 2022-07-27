fad9225622072fe4be6007030765ba30b3ec0ff4
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-27 08:42:31 -0700

    Update 1-strdup.c file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,14 +12,14 @@ char *_strdup(char *str)
 	int i;
 
 	i = 0;
+	if (str == NULL)
+		return (NULL);
 	while (str[i])
 	{
 		i++;
 	}
 	strp = (char *) malloc(i + 1);
 	if (strp == NULL)
-		return (NULL);
-	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
