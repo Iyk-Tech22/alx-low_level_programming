b680e8f60143ba3c30a83da6efc9cab6e9e15b96
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-07-28 15:42:48 -0700

    Update 2-calloc.c file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,13 +10,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem_alloc;
+	unsigned int i;
+	char *mem_alloc;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	mem_alloc = malloc(nmemb * size);
 	if (mem_alloc == NULL)
 		return (NULL);
-	*(int *)mem_alloc = 0;
+	for (i = 0; i < nmemb; i++)
+		mem_alloc[i] = 0;
 	return (mem_alloc);
 }
