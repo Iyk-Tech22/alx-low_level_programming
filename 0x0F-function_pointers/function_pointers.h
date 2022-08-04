7fcbea28b8c221bcaaccb525e96c621502822e23
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-08-03 23:11:08 -0700

    Update function_pointer.h file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTION_PIONTER_H
 #define _FUNCTION_PIONTER_H
 
+#include <stdlib.h>
 void print_name(char *name, void(*func)(char *n));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
