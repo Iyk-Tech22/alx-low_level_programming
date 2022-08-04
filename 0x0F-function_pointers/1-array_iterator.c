f6e406b40cf62a37fde6653d1a6e5a221c37cbc6
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-08-03 23:27:00 -0700

    Update function_pointer.h file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	i = 0;
-	while (i < size)
+	if (array != (void *)0 && action != (void *)0)
 	{
-		action(array[i]);
-		i++;
+		i = 0;
+		while(i < size)
+		{
+			action(array[i]);
+			i++;
+		}
 	}
 }
