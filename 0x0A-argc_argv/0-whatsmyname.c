e17ede862c3103d4679408fc0ce1ec0a6586ec5e
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-08-07 15:38:06 -0700

    Update whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,12 @@
  *
  * Return: 0 for successful run
  */
-int main(int argc, char *argv[]) {
-  if (argc > 0)
-  {
-    printf("%s\n", argv[0]);
-    }
-  return (0);
-} 
+int main(int argc, char *argv[])
+{
+
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
