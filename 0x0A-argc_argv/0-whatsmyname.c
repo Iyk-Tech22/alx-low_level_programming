4f61d1b3301b4d8a66bcc4c2616300346266e17b
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-08-07 15:26:59 -0700

    Create 0-whatsmyname.c  file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point of the Program
+ * @argc: Number of args
+ * @argv: Array of args
+ *
+ * Return: 0 for successful run
+ */
+int main(int argc, char *argv[]) {
+  if (argc > 0)
+  {
+    printf("%s\n", argv[0]);
+    }
+  return (0);
+} 
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+#cisfun starting new project
