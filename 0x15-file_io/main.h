a7df7cdbcb9d6536e3e09a7d0b6d02f42cf89998
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2022-08-23 04:23:27 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,10 @@
 
 #include <stddef.h>
 #include <unistd.h>
-ssize_t read_textfile(const char *, size_t);
+#define BUFSIZE 1204
+ssize_t read_textfile(const char *, size_t
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 #endif
