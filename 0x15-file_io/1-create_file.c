67e69979dc4c0de58ab96f9cdaef77ea48eb086b
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2022-08-23 04:41:47 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (file == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 	{
 		while (text_content[len] != '\0')
 			len++;
