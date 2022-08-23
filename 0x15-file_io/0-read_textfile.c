9a388f2aae2db68f7a0b44fdb9b6d30962df599f
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-08-23 09:34:56 -0700

    Update 0-read_textfile :) 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stddef.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - Read text file from storage
@@ -13,20 +14,23 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 
-	int fd;
-	char buffer[letters];
+	int fd, r_write, r_read;
+	char *buffer;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	letters = read(fd, buffer, letters);
+	buffer = malloc(letters);
+	r_read = read(fd, buffer, letters);
+	if (r_read == -1)
+		return (0);
 	buffer[letters] = '\0';
 	close(fd);
 	/* write to standard output */
-	letters = write(STDOUT_FILENO, buffer, letters);
-	if (letters == -1)
+	r_write = write(STDOUT_FILENO, buffer, letters);
+	if (r_write == -1)
 		return (0);
-	return (letters);
+	return (r_write);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <unistd.h>
 #define BUFSIZE 1204
-ssize_t read_textfile(const char *, size_t
+ssize_t read_textfile(const char *, size_t);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
